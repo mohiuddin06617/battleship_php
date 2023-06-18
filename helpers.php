b9ccbe8c08e7a7c75c32b965c0e8dad885bfbb53
mohiuddin06617 <mohiuddin06617@gmail.com>
2023-06-18 17:48:19 +0600

    Array slicing method added

--- helpers.php
+++ helpers.php
@@ -1,1 +1,10 @@
 <?php
+function getArrayValuesByRange($board, $startRow, $startCol, $endRow, $endCol) {
+    $arraySlice = [];
+
+    for ($row = $startRow; $row <= $endRow; $row++) {
+        $arraySlice = array_merge($arraySlice, array_slice($board[$row], $startCol, $endCol - $startCol + 1));
+    }
+
+    return $arraySlice;
+}
