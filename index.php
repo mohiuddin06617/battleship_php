579a8f2dcf5706a0a3842e9e0447c9b8ced91a68
mohiuddin06617 <mohiuddin06617@gmail.com>
2023-06-18 17:50:54 +0600

    Shot to sgip method added

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
-<!--Battleship Single Player Game Against Computer
-
-Step 1 - Create two 10x10 boards. One board is Hidden and other is visible to the user.
-Step 2 - Place the ships on the grid. The hidden board holds the locations of the ships marked with X. Guess board will hold '.' value.
-Step 3 - Before placing each ships ensure that it does not overlap with other ships. They can touch but can not overlap. Additionallu, ensure that they are placed within the grid.
-Step 5 - Display the guess board to the user.
-Step 6 - Prompt the user to enter coordinates in the format "A5" to target a square on the grid. Validate inputted coordiates.
-Step 7 - If user entered "show" command then display the hidden board to user alongside with guess board
-Step 8 - Update the guess board according to the users inputted value.
-Step 8.1 -> If the target square is miss (marked as "-") or hit (marked as "X"), then inform the player that it is already guessed.
-Step 8.2 -> If the target square is empty (marked as '.'), update it to a miss marker '-' and inform the player.
-Step 8.3 -> If the target square in hidden board contains a ship, update the guess board marker to 'X'. After each hit, check if all the squares of a ship is hit mark. If it is, then mark it as sunk and inform the player accordingly.
-Step 9 - Keep count of number of shots. Increase number of shots after each iteration.
-Step 10 - Check if game is over. When the total number of ships is equal to sunked ships end the game and notify user that he has won after X number of shots.
--->
-
 <?php
+/*
+ * Battleship Single Player Game Against Computer
+ * Step 1 - Create two 10x10 boards. One board is Hidden and other is visible to the user.
+ * Step 2 - Place the ships on the grid. The hidden board holds the locations of the ships marked with X. Guess board will hold '.' value.
+ * Step 3 - Before placing each ships ensure that it does not overlap with other ships. They can touch but can not overlap. Additionallu, ensure that they are placed within the grid.
+ * Step 5 - Display the guess board to the user.
+ * Step 6 - Prompt the user to enter coordinates in the format "A5" to target a square on the grid. Validate inputted coordiates.
+ * Step 7 - If user entered "show" command then display the hidden board to user alongside with guess board
+ * Step 8 - Update the guess board according to the users inputted value.
+ * Step 8.1 -> If the target square is miss (marked as "-") or hit (marked as "X"), then inform the player that it is already guessed.
+ * Step 8.2 -> If the target square is empty (marked as '.'), update it to a miss marker '-' and inform the player.
+ * Step 8.3 -> If the target square in hidden board contains a ship, update the guess board marker to 'X'. After each hit, check if all the squares of a ship is hit mark. If it is, then mark it as sunk and inform the player accordingly.
+ * Step 9 - Keep count of number of shots. Increase number of shots after each iteration.
+ * Step 10 - Check if game is over. When the total number of ships is equal to sunked ships end the game and notify user that he has won after X number of shots.
+ * */
 require_once "ship.php";
 require_once "board.php";
 
@@ -67,7 +66,13 @@ while (!$board->gameOver) {
     }
 
     [$row, $column] = $coordinates;
+    // TODO: Shot to Ship in here
+    $board->shotToShip($row, $column);
+    $board->shots++;
 
-    print_r($row, $column);
+    // TODO: Check for game over
+
 }
+
+$board->showGuessBoard();
 ?>
