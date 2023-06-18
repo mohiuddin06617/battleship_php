6b4468707f4e162bef465bc40e6762b9edfd653e
mohiuddin06617 <mohiuddin06617@gmail.com>
2023-06-18 15:24:40 +0600

    Validation before placing ship implemented. Ship placement process implemented.

--- board.php
+++ board.php
@@ -28,6 +28,67 @@ class Board
         $this->shots = 0;
     }
 
+
+    public function validateShipPlacementGrid($ship, $row, $column, $orientation) {
+        if ($orientation === "horizontal") {
+            $columnSize = $column + $ship->size;
+            if ($columnSize > 10) {
+                return false;
+            }
+            for ($j = $column; $j < $columnSize; $j++) {
+                if ($this->hiddenBoard[$row][$j] !== ".") {
+                    return false;
+                }
+            }
+        } else {
+            $rowSize = $row + $ship->size;
+            if ($rowSize > 10) {
+                return false;
+            }
+            for ($i = $row; $i < $rowSize; $i++) {
+                if ($this->hiddenBoard[$i][$column] !== ".") {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public function placeShip($ship, $row, $column, $orientation) {
+        if ($orientation === "horizontal") {
+            $endCol = 0;
+            for ($j = $column; $j < $column + $ship->size; $j++) {
+                $this->hiddenBoard[$row][$j] = "X";
+                $endCol = $j;
+            }
+            $this->shipCoordinates[] = [$column, $endCol, $row, $orientation];
+        } else {
+            $endRow = 0;
+            for ($i = $row; $i < $row + $ship->size; $i++) {
+                $this->hiddenBoard[$i][$column] = "X";
+                $endRow = $i;
+            }
+            $this->shipCoordinates[] = [$row, $endRow, $column, $orientation];
+        }
+
+        $this->ships[] = $ship;
+        $this->numOfShipsPlaced++;
+    }
+
+    public function shipPlacementProcess($ship) {
+        while (true) {
+            $row = rand(0, 9);
+            $column = rand(0, 9);
+            $orientation = (rand(0, 1) === 0) ? "horizontal" : "vertical";
+            // TODO: Ship place validation in here
+            if ($this->validateShipPlacementGrid($ship, $row, $column, $orientation)) {
+                // TODO: Ship placement here
+                $this->placeShip($ship, $row, $column, $orientation);
+                break;
+            }
+        }
+    }
+
     public function showHiddenBoard() {
         echo "Hidden Board:\n";
         $this->showBoard($this->hiddenBoard);
@@ -45,3 +106,4 @@ class Board
         }
     }
 }
+?>
