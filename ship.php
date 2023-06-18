7990bf516eb36b1499abff3e0bb35c326e8c0efd
mohiuddin06617 <mohiuddin06617@gmail.com>
2023-06-18 14:24:37 +0600

    Ship structure defined

--- ship.php
+++ ship.php
@@ -1,1 +1,12 @@
 <?php
+class Ship {
+    public $name;
+    public $size;
+    public $hits;
+
+    public function __construct($name, $size) {
+        $this->name = $name;
+        $this->size = $size;
+        $this->hits = 0;
+    }
+}
